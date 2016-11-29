f24b68abf8f1e7c855782cf25eded9abd30c962f
nhodge253 <nhodge253@gmail.com>
2016-11-29 03:41:31 -0500

    added crew page and tinkered with media queries

--- js/main.js
+++ js/main.js
@@ -5,3 +5,13 @@ if($('.check-media').width() == 768){
 }else{
     console.log('You are out of (max-width: 768px)');
 }
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    var x = document.getElementById("nav");
+    if (x.className === "sf-menu") {
+        x.className += " responsive";
+    } else {
+        x.className = "sf-menu";
+    }
+}
